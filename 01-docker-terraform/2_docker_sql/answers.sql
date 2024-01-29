b33fe07ceb24823a731b875f14ac61e47223d5f7
quezon <gregoryallegory@gmail.com>
2024-01-28 19:17:31 -0500

    Copied files from old dir to new dir

---
+++ 01-docker-terraform/2_docker_sql/answers.sql
@@ -0,0 +1,32 @@
+-- Question 3 method 1
+SELECT count(*) FROM green_taxi_trips
+WHERE lpep_pickup_datetime BETWEEN timestamp '2019-09-18 00:00:00' AND timestamp '2019-09-18 23:59:59' 
+AND lpep_dropoff_datetime BETWEEN timestamp '2019-09-18 00:00:00' AND timestamp '2019-09-18 23:59:59';
+-- Question 3 method 2
+SELECT count(*) FROM green_taxi_trips
+WHERE lpep_pickup_datetime >= timestamp '2019-09-18 00:00:00'
+AND lpep_dropoff_datetime <= timestamp '2019-09-18 23:59:59';
+
+
+-- Question 4
+select lpep_dropoff_datetime - lpep_pickup_datetime td, * from green_taxi_trips
+where lpep_pickup_datetime IN 
+(timestamp '2019-09-18',timestamp '2019-09-16',timestamp '2019-09-26',timestamp '2019-09-21')
+order by td desc limit 1;
+
+
+-- Question 5
+select b."Borough", count(*) from public.green_taxi_trips a
+inner join zones b on (a."PULocationID" = b."LocationID")
+where b."Borough" <> 'Unknown'
+group by b."Borough"
+having count(*) > 50000;
+
+
+-- Question 6
+select b."Zone" as "From", c."Zone" as "To", a.tip_amount from public.green_taxi_trips a
+inner join zones b on (a."PULocationID" = b."LocationID")
+inner join zones c on (a."DOLocationID" = c."LocationID")
+where b."Zone" = 'Astoria'
+order by a.tip_amount desc
+limit 1;
